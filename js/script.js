87f29507443b0d820988d346841c641f4d4181f6
GenyaHoshino <flandrescarlet@koumakan.vip>
2016-09-20 13:23:21 +0800

    clipBlurBorad
    add function clipblurborad

--- js/script.js
+++ js/script.js
@@ -4,17 +4,25 @@ $(document).ready(function() {
 
 	initPage();
 	initPlayer();
-	
+
 });
 
 
 function initPage(){
-	
-	
+
+
 	bg = parseInt(Math.random()*(bgImg.length));
-	$('body,.header,.footer').css({
+	$('.container-fluid,.header,.footer').css({
 		background: 'url("img/'+bgImg[bg]+'") center/cover no-repeat',
 	 });
+	// $square = $('.square');
+	// for(i = 0; i < $square.length; i++)
+	// {
+	// 	$square.eq(i).css({
+	// 		background: 'rgba('+parseInt(Math.random()*255)+','+parseInt(Math.random()*255)+','+parseInt(Math.random()*255)+',0.4)',
+	// 		height: $square.eq(i).outerWidth()
+	// 	});
+	// }
 	resizePage();
 	$(window).resize(function(event) {
 		resizePage();
@@ -24,29 +32,104 @@ function initPage(){
 
 
 function resizePage(){
+		$('.contentClip>div').remove();
 		wHeight = $(window).height();
 		wWidth = $(window).width();
-		$('body').css({
+		$('.container-fluid').css({
 			height : wHeight+'px'
 		});
 		$('.header,.footer').css({
-			height : wHeight+10+'px',
-			width : wWidth+'px'
+			height : wHeight+20+'px'
 		});
-		// $('.content,.contentBg').css({
-		// 	height: wHeight * 0.7,
-		// 	top : wHeight * 0.15
-		// });
-		$('.profile,.profileBg').css({
-			height: wHeight,
-		});
+		 // clip($('.header'));
 		$('.header').css({
-			clip : 'rect(0px,'+wWidth+'px,'+0.1*wHeight+'px,0px)'
+			clip : 'rect(10px,'+wWidth+'px,'+0.075*wHeight+'px,0px)'
 		});
 		$('.footer').css({
-			clip : 'rect('+0.9*wHeight+'px,'+wWidth+'px,'+wHeight+'px,0px)'
+			clip : 'rect('+(0.9*wHeight)+'px,'+wWidth+'px,'+wHeight+'px,0px)'
 		});
+
+		$('.headerShadow').css({
+			height : wHeight * 0.075+'px'
+		});
+		$('.footerShadow').css({
+			height : wHeight * 0.1 +'px',
+			top : (wHeight * 0.9 -10 )  +'px'
+		});
+
+
+		$('.content').css({
+			top : wHeight * 0.1 + 'px',
+		});
+		$('.contentClip').css({
+			height: wHeight,
+			width: wWidth
+		});
+
+		if(wWidth >= 992){
+			$('.articles').css({
+				height: 0.75 * wHeight,
+				'margin-top' : 0
+			});
+		}
+		else{
+			$('.articles').css({
+				'margin-top' : '15px'
+			});
+		}
+
+		for(i = 0; i < $('.content>.row').children().length; i++)
+		{
+			clip($('.content>.row').children()[i].classList[0],wWidth,wHeight);
+		}
+		clip('menu',wWidth,wHeight);
+
+		// $('.content,.profile').css({
+		// 	height: wHeight * 0.75 + 'px'
+		// });
+
+
+
+
 }
+
+
+function clip(className,wWidth,wHeight){
+	$model = $('.'+className);
+	oTop = $model.offset().top - parseInt($model.css('top').substr(0,$model.css('top').lastIndexOf("px"))) ;
+	oBottom = oTop + $model.height();
+	oLeft = $model.offset().left - parseInt($model.css('left').substr(0,$model.css('left').lastIndexOf("px"))) + ($model.outerWidth() - $model.width())/2;
+	oRight = oLeft + $model.width();
+	$blur = $('<div class="'+className+'Blur contentBlur"></div>').load();
+	$shadow = $('<div class="'+className+'Shadow contentShadow"></div>').load();
+	$blur.css({
+		position: 'absolute',
+		clip: 'rect('+oTop+'px,'+oRight+'px,'+oBottom+'px,'+oLeft+'px)',
+		height : $('.contentClip').height(),
+		background: 'url("img/'+bgImg[bg]+'") center/cover no-repeat',
+		width:wWidth,
+		'z-index':0
+	});
+	$shadow.css({
+		position: 'absolute',
+		height: $model.height(),
+		width: $model.width(),
+		top : oTop,
+		left : oLeft
+	});
+	$('.contentClip').append($blur).append($shadow);
+}
+
+
+/*
+	list
+
+
+
+
+*/
+
+
 
 // var num = 0;
 // var arrTxt = new Array();
